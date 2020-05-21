cf1f2b7a3f8200a98c5ba5744c9e2c6ba9599385
hollandaise1 <xinzhuli.cmu@gmail.com>
2020-05-20 22:46:15 -0700

    Update amz_BIE.py

--- amz_BIE.py
+++ amz_BIE.py
@@ -1,6 +1,7 @@
 """To write a function that count the odd occurence of integers in an array """
 arr = [2,2,4,3,5,4,5,4,5,3,2,2]
 
+#Solution 1:
 def getOccurance(myArray):
     counterDict = {}
     for item in myArray:
@@ -21,3 +22,15 @@ def oddMod(myDict):
     return newDict
 
 #Expected Results: {4: 3, 5: 3}
+
+#Solution 2:
+import collections
+arr = [2,2,4,3,5,4,5,4,5,3,2,2]
+num2count = collections.Counter(arr)
+print(num2count)
+odd_nums = [x for x in num2count if num2count[x]%2]
+print(odd_nums)
+
+#Counter({2: 4, 4: 3, 5: 3, 3: 2})
+#[4, 5]
+
